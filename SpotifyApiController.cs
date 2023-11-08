a3098fae29984f043afb1a65c53dcad13361f8ac
EthanWakeford <ethanwakeford@gmail.com>
2023-11-07 21:01:53 -0600

    figuring out how to send http requests

--- SpotifyApiController.cs
+++ SpotifyApiController.cs
@@ -1,6 +1,16 @@
 // class for interfacing with the spotify REST API
 
 namespace v2_spotify_app.SpotifyApiController;
+
+public class SpotifyHttpClient : HttpClient
+{
+    public SpotifyHttpClient()
+    {
+        BaseAddress = new Uri("https://accounts.spotify.com/api");
+
+    }
+}
+
 public class Spotify
 {
 
@@ -11,4 +21,14 @@ public class Spotify
         this.clientId = clientId;
         this.clientSecret = clientSecret;
     }
+
+    // public async string CreateAuthCode()
+    // {
+    //     // creates an auth code with spotify API oauth auth code flow
+    //     const string url = "https://accounts.spotify.com/api/token";
+    //     // const Dictionary<string, string> headers = new Dictionary<string, string>() { { "Content-Type", "application/x-www-form-urlencoded" } };
+    //     var client = new HttpClient();
+    // var message = new HttpRequestMessage();
+    //     return;
+    // }
 }
