c8d178ce9d42e57f38c0d4e59a72c8bf2ccf14fe
EthanWakeford <ethanwakeford@gmail.com>
2023-11-10 13:26:14 -0600

    finally got the token request to work

--- SpotifyApiController.cs
+++ SpotifyApiController.cs
@@ -29,22 +29,30 @@ public class Spotify
     {
         // creates an auth code with spotify API oauth auth code flow
         const string uri = "https://accounts.spotify.com/api/token";
-        var requestMessage = new HttpRequestMessage(HttpMethod.Post, new Uri(uri));
-        requestMessage.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+
+        var postData = new Dictionary<string, string>{
+            {"grant_type", "client_credentials"},
+            {"client_id", clientId},
+            {"client_secret", clientSecret},
+        };
+
+        var requestMessage = new HttpRequestMessage(HttpMethod.Post, new Uri(uri))
+        {
+            Content = new FormUrlEncodedContent(postData)
+        };
 
         HttpResponseMessage response = await spotifyClient.SendAsync(requestMessage);
+        string? responseString = await response.Content.ReadAsStringAsync();
 
-        Console.WriteLine(response.StatusCode.ToString(), response);
-
-        Console.WriteLine(response.Content.ToString());
-        // var response = await 
-        var responseString = response.Content.ToString();
+        // debugging stuff, remove later
+        Console.WriteLine($"the whole response object: {response}");
+        Console.WriteLine($"response content: {responseString}");
 
         if (responseString == null)
         {
             return $"it broke, here's why {response.StatusCode}";
         }
-        return response.Content.ToString();
+        return responseString;
     }
 
     // public async Task<string?> getArtist()
